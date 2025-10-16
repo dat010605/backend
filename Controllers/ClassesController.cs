a3de83375d3fab0d17486f2255f2e63e1765216a
khanh67 <duongduykhanh06072005@gmail.com>
2025-10-16 18:25:14 +0700

    cap nhap

--- Controllers/ClassesController.cs
+++ Controllers/ClassesController.cs
@@ -22,39 +22,26 @@ namespace StudentClassApi.Controllers
 
         // GET: api/classes
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ClassDto>>> GetClasses()
-        {
-            var classes = await _context.Classes.Include(c => c.Students).ToListAsync();
-            var classDtos = _mapper.Map<IEnumerable<ClassDto>>(classes);
-            return Ok(classDtos);
-        }
+        public async Task<ActionResult<IEnumerable<ClassDto>>> GetClasses() =>
+            Ok(_mapper.Map<IEnumerable<ClassDto>>(
+                await _context.Classes.Include(c => c.Students).ToListAsync()
+            ));
 
-        // ✅ POST: api/classes
+        // POST: api/classes
         [HttpPost]
-        public async Task<ActionResult<ClassDto>> CreateClass(CreateClassDto createClassDto)
+        public async Task<ActionResult<ClassDto>> CreateClass(CreateClassDto dto)
         {
-            var newClass = new Class
-            {
-                Name = createClassDto.Name
-            };
-
+            var newClass = _mapper.Map<Class>(dto);
             _context.Classes.Add(newClass);
             await _context.SaveChangesAsync();
-
-            var classDto = _mapper.Map<ClassDto>(newClass);
-            return Ok(classDto);
+            return Ok(_mapper.Map<ClassDto>(newClass));
         }
 
         // GET: api/classes/{classId}/students
         [HttpGet("{classId}/students")]
-        public async Task<ActionResult<IEnumerable<StudentDto>>> GetStudentsInClass(int classId)
-        {
-            var students = await _context.Students
-                .Where(s => s.ClassId == classId)
-                .ToListAsync();
-
-            var studentDtos = _mapper.Map<IEnumerable<StudentDto>>(students);
-            return Ok(studentDtos);
-        }
+        public async Task<ActionResult<IEnumerable<StudentDto>>> GetStudentsInClass(int classId) =>
+            Ok(_mapper.Map<IEnumerable<StudentDto>>(
+                await _context.Students.Where(s => s.ClassId == classId).ToListAsync()
+            ));
     }
 }
